7f39cfe416433a001f5d4bb5a2ce44e035135a47
Serenity24 <132241276+Jacob-Mullen@users.noreply.github.com>
2023-09-19 23:02:56 +1200

    version final

--- testsog/routes.py
+++ testsog/routes.py
@@ -4,24 +4,20 @@ import sqlite3
 
 app =  Flask(__name__)
 
-@app.route('/swing')
-def home():
+
+
+
+#this is the route to the home page/landing page
+@app.route('/')
+def landingpage():
     conn = sqlite3.connect('song.db')
     cur = conn.cursor()
-    cur.execute('SELECT * FROM song WHERE feel=1 ')
-    results = cur.fetchall()
+    cur.execute('SELECT * FROM song WHERE feel=4 ')
+    results = cur.fetchone()
     print (results)
-    return render_template("swing.html", title = "swing",results=results)
+    return render_template('landingpage.html',results=results)
 
-@app.route('/bossa')
-def contact():
-    conn = sqlite3.connect('song.db')
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM song WHERE feel=2 ')
-    results = cur.fetchall()
-    print (results)
-    return render_template("bossa.html", title = "bossa",results=results)
-
+#this is the route to the standerds page whitch will display magority of the information in the database
 @app.route('/stand/<int:id>')
 def stand(id):
     conn = sqlite3.connect('song.db')
@@ -37,32 +33,47 @@ def stand(id):
     print(link)
     return render_template("stand.html", title = "stand",results=results, image=image, link=link )
 
-@app.route('/beebop')
+#this is the route to the swing page witch will display all the songs that are in the catagry of swing
+@app.route('/swing')
+def home():
+    conn = sqlite3.connect('song.db')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM song WHERE feel=1 ')
+    results = cur.fetchall()
+    print (results)
+    return render_template("swing.html", title = "swing",results=results)
+
+#this is the route to the bossa page witch will display all the songs in the catagry of bossa nova
+@app.route('/bossa')
+def contact():
+    conn = sqlite3.connect('song.db')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM song WHERE feel=2 ')
+    results = cur.fetchall()
+    print (results)
+    return render_template("bossa.html", title = "bossa",results=results)
+
+#this is the route to the bebop page it is used to display all the bebop catagry songs
+@app.route('/bebop')
 def about():
     conn = sqlite3.connect('song.db')
     cur = conn.cursor()
     cur.execute('SELECT * FROM song WHERE feel=3 ')
     results = cur.fetchall()
     print (results)
-    return render_template("beebop.html", title = "samba",results=results)
+    return render_template("bebop.html", title = "samba",results=results)
 
-@app.route('/feels')
+#this i the others route it is used to diplay the songs that dont fit in the other three category 
+@app.route('/other')
 def feels():
     conn = sqlite3.connect('song.db')
     cur = conn.cursor()
     cur.execute('SELECT * FROM song WHERE feel=4 ')
     results = cur.fetchall()
     print (results)
-    return render_template('feels.html',results=results)
+    return render_template('other.html',results=results)
 
-@app.route('/landingpage')
-def landingpage():
-    conn = sqlite3.connect('song.db')
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM song WHERE feel=4 ')
-    results = cur.fetchone()
-    print (results)
-    return render_template('landingpage.html',results=results)
+
 
 
 if __name__ == "__main__":
